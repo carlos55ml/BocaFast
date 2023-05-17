15411e2a8baf60783f51d4908f91b6fc15e77838
carlos55ml <cmoranlozano55@gmail.com>
2023-05-17 22:04:37 +0200

    wip: piece view

---
+++ bocafast-ui/src/templates/admin/pieces/piece.vue
@@ -0,0 +1,42 @@
+<script setup>
+import NavBar from '../components/NavBar.vue';
+</script>
+
+<script>
+import emitter from './../assets/js/emitter'
+import {} from './../assets/js/adminHelper'
+export default {
+  data() {
+    return {
+      user: {},
+    };
+  },
+  mounted() {
+    emitter.on('userObj', userObj =>{ this.user = userObj; this.checkAdmin() })
+    this.setPageData()
+  },
+  methods: {
+    checkAdmin() {
+      if(!this.user || !this.user.admin == 1) {
+        alert("Necesitas ser administrador para ver esta pagina.")
+        window.location = '/'
+      }
+    },
+    async setPageData() {}
+  }
+}
+</script>
+
+<template>
+  <NavBar></NavBar>
+
+  <main class="uk-container uk-align-center" v-if="user && user.admin" :key="user">
+    <h1 class="uk-text-center">Bocata</h1>
+    
+  </main>
+</template>
+
+<style scoped>
+
+</style>
+
